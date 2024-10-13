1899f92d5bb4907c56158cb125e9c42613c1cc2a
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-10-12 22:17:24 -0700

    Add files via upload

---
+++ pointer/pointer10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+ 
+int main() {
+    int num = 5;
+     
+    printf("Factorial of %d: %d\n", num, factorial(num));
+     
+    return 0;
+}
