43aea7597d181fe7b95a211e0968db99762c3520
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-10-18 03:45:19 -0700

    Add files via upload

---
+++ string/string3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char nickname[20];
+ 
+    puts("Enter your Nick name:");
+ 
+    gets(nickname);
+ 
+    puts(nickname);
+ 
+    return 0;
+}
