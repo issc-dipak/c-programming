39684edd173d49933b23c4b468c78a51b0b44d19
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2024-11-05 06:15:06 -0800

    Add files via upload

--- struct/struct.c
+++ struct/struct.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
-#include <string.h>
-
-// create struct with person1 variable
-struct Person {
-  char name[50];
-  int citNo;
-  float salary;
-} person1;
-
 int main() {
-
-  // assign value to name of person1
-  strcpy(person1.name, "George Orwell");
-
-  // assign values to other person1 variables
-  person1.citNo = 1984;
-  person1. salary = 2500;
-
-  // print struct variables
-  printf("Name: %s\n", person1.name);
-  printf("Citizenship No.: %d\n", person1.citNo);
-  printf("Salary: %.2f", person1.salary);
-
-  return 0;
+    char operator;
+    double num1, num2;
+    printf("Enter operator (+, -, *, /): ");
+    scanf("%c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &num1, &num2);
+    switch(operator) {
+        case '+': printf("%.2lf\n", num1 + num2); break;
+        case '-': printf("%.2lf\n", num1 - num2); break;
+        case '*': printf("%.2lf\n", num1 * num2); break;
+        case '/': printf("%.2lf\n", num1 / num2); break;
+        default: printf("Error! operator is not correct\n");
+    }
+    return 0;
 }
