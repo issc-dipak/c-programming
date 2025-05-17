f8fbff582c9bc94512105a1fbca8ef6be7188017
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-05-17 06:06:27 -1200

    Create 1to10.c

---
+++ 1to10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from C!\n");
+
+    // Print numbers from 1 to 20
+    for (int i = 1; i <= 20; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
